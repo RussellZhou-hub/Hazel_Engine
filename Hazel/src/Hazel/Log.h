11806ac7e04e7956856ef5d5bd5d63b35afcef3b
RussellZhou-hub <938311288@qq.com>
2024-07-13 01:02:15 +0800

    Event system fixed

--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "spdlog/spdlog.h"
+#include "spdlog/fmt/ostr.h"
 
 namespace Hazel {
 	class HAZEL_API Log
@@ -24,14 +25,14 @@ namespace Hazel {
 #define HZ_CORE_ERROR(...)	::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__)
 #define HZ_CORE_WARN(...)	::Hazel::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define HZ_CORE_INFO(...)	::Hazel::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define HZ_CORE_TRACE(...)	::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define HZ_CORE_TRACE(...)	::Hazel::Log::GetCoreLogger()->trace(__VA_ARGS__)
 #define HZ_CORE_FATAL(...)	::Hazel::Log::GetCoreLogger()->fatal(__VA_ARGS__)
 
 // Client log macro
 #define HZ_ERROR(...)	::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
 #define HZ_WARN(...)	::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define HZ_INFO(...)	::Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
-#define HZ_TRACE(...)	::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
-#define HZ_FATAL(...)	::Hazel::Log::GetClientLogger()->fatal(__VA_ARGSlient
+#define HZ_TRACE(...)	::Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define HZ_FATAL(...)	::Hazel::Log::GetClientLogger()->fatal(__VA_ARGS__)
 
 
